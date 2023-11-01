825cb67b6c909506b94b4e06eac497e715ac80c6
penguin <penguin@hepi.co.kr>
2023-11-01 16:46:57 +0900

    add sample for test

--- sample/index.ts
+++ sample/index.ts
@@ -2,9 +2,12 @@ import value = require("./moduleexport");
 import value2 = require("./moduleexport2");
 import "./declaremodule";
 import "./jsfile";
+import * as fs from "fs/promises";
 require("arg");
 
 export let x = 0;
+
+fs.readFile("");
 
 let b: number = value.v;
 console.log(value2);
